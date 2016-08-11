824dbaff44fe87335648cac1cacbcf539a822834
Lee Byron <lee@leebyron.com>
2016-08-11 14:35:31 -0700

    fix register hook

--- register.js
+++ register.js
@@ -6,7 +6,7 @@ var prev = require.extensions['.js'];
 require.extensions['.js'] = function(module, filename) {
   if (filename.indexOf('node_modules') === -1) {
     var source = fs.readFileSync(filename, 'utf8');
-    module._compile(asyncToGen(source), filename);
+    module._compile(asyncToGen(source).toString(), filename);
   } else if (prev) {
     prev(module, filename);
   } else {

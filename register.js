118e383f81e8b9eb0b32d348e42401ec18fd59af
Vladislav Botvin <dark-vlados@mail.ru>
2016-09-17 02:24:50 +0300

    allow pass options to register hook

--- register.js
+++ register.js
@@ -4,10 +4,12 @@ var asyncToGen = require('./index');
 // Rather than use require.extensions, swizzle Module#_compile. Not only does
 // this typically leverage the existing behavior of require.extensions['.js'],
 // but allows for use alongside other "require extension hook" if necessary.
-var super_compile = Module.prototype._compile;
-Module.prototype._compile = function _compile(source, filename) {
-  var transformedSource = filename.indexOf('node_modules/') === -1
-    ? asyncToGen(source).toString()
-    : source;
-  super_compile.call(this, transformedSource, filename);
+module.exports = function(options) {
+  var super_compile = Module.prototype._compile;
+  Module.prototype._compile = function _compile(source, filename) {
+    var transformedSource = filename.indexOf('node_modules/') === -1
+      ? asyncToGen(source, options).toString()
+      : source;
+    super_compile.call(this, transformedSource, filename);
+  };
 };
